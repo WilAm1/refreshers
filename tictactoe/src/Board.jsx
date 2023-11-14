366a613729bc69970b7fa0b89f4f0b22f8f7d064
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-11-14 15:30:26 +0800

    add: logic to the board

--- tictactoe/src/Board.jsx
+++ tictactoe/src/Board.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Square({ value, onSquareClick }) {
   function handleClick() {
     onSquareClick();
-    console.log(value);
   }
 
   return (
@@ -13,18 +12,83 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+const checkWinner = function (squares) {
+  console.log(squares);
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // squares represent the board array, the 9 filled array
+  // check if it accomplishes the work.
+  // lines represent the combinations, in array index.
+
+  /**
+   * I want to check then the squares if it matches the indexes
+   *
+   * I want to loop through the lines array
+   *
+   * check the first number, the second number, and the third number if all of them are in the same symbol.
+   */
+
+  for (let i = 0; i < lines.length; i++) {
+    const first = lines[i][0];
+    const second = lines[i][1];
+    const third = lines[i][2];
+
+    if (
+      // check if all of them are in the same combination
+      squares[first] &&
+      // check if it is not null
+      squares[first] === squares[second] &&
+      squares[second] === squares[third]
+    ) {
+      console.log("you win!");
+    }
+  }
+};
+
 export default function Board() {
   const [val, setVal] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState("X");
 
+  // changes the value of the array by renaming it to X
   function handleClick(num) {
     // clone the array
     const newArr = val.slice();
+
     // fill the modified array
-    newArr[num] = "x";
+    newArr[num] = turn;
+
+    // change the turn
+    setTurn(turn === "O" ? "X" : "O");
+
+    // set new array. then check if there is a winner!
 
     // return new array
     setVal(newArr);
   }
+
+  useEffect(() => {
+    checkWinner(val);
+  });
+
+  // check if it is complete
+  /**
+
+  *   x , x, x
+   *  null, null, null
+   * null, null, null
+   */
+
+  // function isComplete() {
+  //   // check the array if there is no null
+  // }
 
   return (
     <>
